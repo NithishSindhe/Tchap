28bd68930b69733fdf5db0bfb8976d43cae9bb18
Nithish Sindhe - nsindhe <nithishsindhe@gmail.com>
2024-09-23 22:55:59 +0530

    bug fixes and enhancements

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React,{useEffect, useState} from 'react'
 import "./App.css"
 import MessageRoom from "./componenets/messageRoom"
+import Virtualizedlist from "./componenets/Virtualizedlist"
 import io from "socket.io-client";
 import Cookies from "universal-cookie"
 import { Navigate } from 'react-router-dom';
@@ -12,44 +13,44 @@ const Cookie = new Cookies();
 
 
 function App(){
-  
-  const [renderRoom,setRenderRoom] = useState(false);
-  const [roomId,setRoomID] = useState();
-  const [userData,setUserData] = useState();
-  
+    const [renderRoom,setRenderRoom] = useState(false);
+    const [roomId,setRoomID] = useState();
+    const [userData,setUserData] = useState();
 
-  const handleRoomId = (element) =>{
+    //return <Virtualizedlist/>
+    const handleRoomId = (element) =>{
     setRoomID(element.target.value);
-  }
+    }
 
-  const joinRoom = ()=>{
+    const joinRoom = ()=>{
     if(roomId){ 
       setRenderRoom(true)
     }else{
       document.getElementById("roomId").placeholder = "Please enter valid room ID"
     };
-  }
+    }
 
-  useEffect(()=>{
+    useEffect(()=>{
     let temp = Cookie.getAll();
     setUserData(temp);
-  },[])
-  if(userData !== undefined){
+    },[])
+    if(userData !== undefined){
     if(userData["loggedIn"] === false || userData["loggedIn"] === undefined){
       return (<Navigate to="/login"></Navigate>);
-  }}
-  return (
+    }}
+    return (
     <div className='wrapper'>
        {(renderRoom)?
        (<MessageRoom userData = {userData} socket = {socket} renderRoom={setRenderRoom} roomId = {roomId}></MessageRoom>):
        (<span className='enterRoom'>
-        <input id = "roomId" type = "text" placeholder='Enter Room Id to join' onChange={handleRoomId}></input>
+        <input className='roomIdInput' id = "roomId" type = "text" placeholder='Enter Room Id to join' onChange={handleRoomId}></input>
         <br/>
-        <button  onClick={joinRoom}>Join or create Room</button>
+        <button className='joinRoom'  onClick={joinRoom}>Join or create Room</button>
+       <iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="150"  className='appleMusic' sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/in/album/show-stop/1533867305?i=1533867503"></iframe>
       </span>
       )}
     </div>
-  );
+    );
 }
 
 export default App;
--- server/db/user.js
+++ server/db/user.js
@@ -1,18 +1,96 @@
 //const mongoose = require("mongoose");
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'TchapDb'
-});
-connection.connect();
+var db_connect_info = {
+      host     : 'localhost',
+      user     : 'root',
+      password : '',
+      database : 'TchapDb'
+    }
 
-const check_mysql_user = (email) => {
-    console.log(email)
-    return false    
+const run_query_command = command => {
+    return new Promise( (resolve,reject) => {
+        var connection = mysql.createConnection(db_connect_info);
+        connection.query(command,(error,results,fields) => {
+            if(error) return reject(error)
+            console.log(results)
+            return resolve(results)
+        })
+    })
 }
 
-module.exports = {check_mysql_user};
+const check_email_existence = (email) => {
+    return new Promise( (resolve,reject) => {
+        command = `SELECT email FROM user_identity WHERE email = '${email}';`
+        var connection = mysql.createConnection(db_connect_info);
+        connection.connect((err) => {
+            if(err) return reject(err);
+            run_query_command(command)
+                .then(response => {
+                    if(response.length == 0) {
+                        return resolve(false)
+                    }
+                    return resolve(true);
+                })
+                .catch(errors => reject(errors))
+            //email_bool = connection.query(query_command, (error,results,fields) => {
+            //    if(error) throw error;
+            //    if(results.length == 0) return false
+            //    return true // return true if email exists in db 
+            //})
+            //console.log('email bool:',email_bool)
+        });
+    })
+    command = `SELECT email FROM user_identity WHERE email = '${email}';`
+    var connection = mysql.createConnection(db_connect_info);
+    connection.connect((err) => {
+        if(err) throw err;
+        run_query_command(command)
+            .then(response => {
+                if(response.length == 0) {
+                    console.log('promise resolved as false')
+                    return false
+                }
+                console.log('promise resolved as true')
+                return true
+            })
+            .catch(errors => console.log('error occured:',errors))
+        //email_bool = connection.query(query_command, (error,results,fields) => {
+        //    if(error) throw error;
+        //    if(results.length == 0) return false
+        //    return true // return true if email exists in db 
+        //})
+        //console.log('email bool:',email_bool)
+    });
+    console.log('returning default false for check email existance')
+    return false // safe to not allow registration in case something goes wrong
+}
+
+const get_user_info = (email) => {
+    command = `SELECT * FROM user_identity where email = '${email}'`
+    var connection = mysql.createConnection(db_connect_info)
+    connection.connect( err => {
+        if(err) throw err
+        connection.query(command, (error,results,fields) => {
+            if(error) throw error
+            console.log(results);
+        });
+    })    
+}
+
+const create_mysql_user = (username, password, email, phone) => {
+    command = `INSERT INTO user_identity (userName, phoneNumber, password, email) VALUES ("${username}", "${phone}", "${password}", "${email}");`
+    if(phone == null) command = command = `INSERT INTO user_identity (userName, phoneNumber, password, email) VALUES ("${username}", ${phone}, "${password}", "${email}");`
+    var connection = mysql.createConnection(db_connect_info);
+    console.log('creating new user')
+    connection.connect((err) => {
+        if(err) throw err
+        connection.query(command, (error,response,fields) => {
+            if(error) throw new Error({name:'Error creating new user:', message:error})
+            console.log('created new user. Response:',response)
+        })
+    })
+}
+
+module.exports = {check_email_existence, create_mysql_user, get_user_info};
 
